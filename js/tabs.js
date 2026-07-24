751741129ee4ce7193855490718b24e509cbb83b
Eric Weubbe <eweubbe8@gmail.com>
2026-07-24 14:11:04 -0500

    Adding hamburger menu for mobile

--- js/tabs.js
+++ js/tabs.js
@@ -4,16 +4,25 @@ class NavTabs extends HTMLElement
   {
     this.innerHTML = `
       <nav class="tab-container">
-        <a href="index.html" class="tab" data-page="index.html">Home</a>
-        <a href="about.html" class="tab" data-page="about.html">About</a>
-        <a href="campaigns.html" class="tab" data-page="campaigns.html">Campaigns</a>
-        <a href="gallery.html" class="tab" data-page="gallery.html">Gallery</a>
-        <a href="testimonials.html" class="tab" data-page="testimonials.html">Testimonials</a>
-        <a href="faqs.html" class="tab" data-page="faqs.html">FAQs</a>
-        <a href="contact.html" class="tab" data-page="contact.html">Contact</a>
+        <button class="hamburger-menu" aria-label="Toggle navigation">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
+        <div class="tabs-list">
+          <a href="index.html" class="tab" data-page="index.html">Home</a>
+          <a href="about.html" class="tab" data-page="about.html">About</a>
+          <a href="campaigns.html" class="tab" data-page="campaigns.html">Campaigns</a>
+          <a href="gallery.html" class="tab" data-page="gallery.html">Gallery</a>
+          <a href="testimonials.html" class="tab" data-page="testimonials.html">Testimonials</a>
+          <a href="faqs.html" class="tab" data-page="faqs.html">FAQs</a>
+          <a href="contact.html" class="tab" data-page="contact.html">Contact</a>
+        </div>
       </nav>
     `;
     this.setActiveTab();
+    this.initMobileMenu();
   }
 
    setActiveTab() 
@@ -31,6 +40,19 @@ class NavTabs extends HTMLElement
         {
             activeLink.classList.add("active");
         }
+  }
+
+  initMobileMenu() 
+  {
+    const burger = this.querySelector('.hamburger-menu');
+    const tabsList = this.querySelector('.tabs-list');
+
+    burger.addEventListener('click', () => 
+    {
+      // Toggle CSS classes to animate the burger and show the dropdown
+      burger.classList.toggle('open');
+      tabsList.classList.toggle('open');
+    });
   }
 }
 
