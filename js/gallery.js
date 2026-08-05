0370260cf574cda01f5bb8ce6e0f1678eb87614a
Eric Weubbe <eweubbe8@gmail.com>
2026-08-05 16:06:00 -0500

    Initial Gallery setup. Need to add smaller thumbnail images.

---
+++ js/gallery.js
@@ -0,0 +1,43 @@
+
+
+const gallery = document.getElementById("gallery");
+
+for (let i = 0; i <= 90; ++i)
+{
+    const img = document.createElement("img");
+
+    img.src = `assets/images/gallery/IMG_${i}.jpg`;
+
+    img.dataset.full = `assets/images/gallery/IMG_${i}.jpg`;
+
+    img.loading = "lazy";
+
+    gallery.appendChild(img);
+}
+
+const lightbox = document.getElementById("lightbox");
+
+gallery.addEventListener("click", (event) =>
+{
+    if (event.target.tagName !== "IMG")
+        return;
+
+    lightboxImage.src = event.target.dataset.full;
+
+    lightbox.classList.remove("hidden");
+});
+
+document
+    .getElementById("closeLightbox")
+    .addEventListener("click", () =>
+{
+    lightbox.classList.add("hidden");
+});
+
+lightbox.addEventListener("click", (event) =>
+{
+    if (event.target === lightbox)
+    {
+        lightbox.classList.add("hidden");
+    }
+});
