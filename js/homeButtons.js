8b1c747e3b2d255c2af14b5f127f617d010c19b7
Eric Weubbe <eweubbe8@gmail.com>
2026-08-21 10:26:39 -0500

    Adding gallery, testimonials, and contact buttons to the home page.

---
+++ js/homeButtons.js
@@ -0,0 +1,14 @@
+const galleryButton = document.getElementById('galleryButton');
+galleryButton.addEventListener('click', () => {
+    window.location.href = 'gallery.html';
+});
+
+const testimonialsButton = document.getElementById('testimonialsButton');
+testimonialsButton.addEventListener('click', () => {
+    window.location.href = 'testimonials.html';
+});
+
+const contactButton = document.getElementById('contactButton');
+contactButton.addEventListener('click', () => {
+    window.location.href = 'contact.html';
+});
